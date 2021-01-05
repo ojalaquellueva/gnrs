01d7dfaaebddc60fad9bb06fcf51853bc42cd427
Brad Boyle <ojalaquellueva@gmail.com>
2021-01-05 14:46:12 -0800

    Update API parameters

--- api/api_params.php
+++ api/api_params.php
@@ -10,6 +10,8 @@ $MAX_ROWS=5000;
 				
 // Default number of batches
 // Makeflow will default to lower number if input lines < $NBATCH
+// Am having issues with lost lines when $NBATCH># cores, so set
+// this number = # of cores, until I can solve issue
 $NBATCH=20;		
 
 // Echo offending SQL on error? (true|false)
